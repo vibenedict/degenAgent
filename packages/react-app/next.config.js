1d9ec50ae63ff79146c37a6997d3a29115ac7ddd
Victor Benedict <victorbenedict43@outlook.com>
2024-11-17 02:05:43 +0700

    Initial commit

---
+++ packages/react-app/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+    };
+    return config;
+  },
+  images: {
+    domains: ['cdn-production-opera-website.operacdn.com'],
+  },
+};
+
+module.exports = nextConfig;
